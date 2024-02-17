a1ea1d6729702a9283840db9b1cdbc456e80bf8c
Daniel Francisco <deny.jo@hotmail.com>
2024-02-17 17:48:54 +0000

    Fix CheatManager server and client view commands for new UI

--- Source/SpaceWarfare/Private/CPP_CheatManager.cpp
+++ Source/SpaceWarfare/Private/CPP_CheatManager.cpp
@@ -5,41 +5,49 @@
 #include "CPP_CameraOrbitController.h"
 #include "CPP_GameHUD.h"
 #include "CPP_Satellite.h"
+#include "CPP_GroundStationPawn.h"
 
 #include "Kismet/GameplayStatics.h"
 #include "Blueprint/UserWidget.h"
+#include "Camera/CameraComponent.h"
 
 
 void UCPP_CheatManager::ServerView()
 {
-    //ACPP_CameraOrbitController* CameraOrbitController = Cast<ACPP_CameraOrbitController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
-    //CameraOrbitController->InputMode = EInputMode::GODMODEINPUT;
+    ACPP_CameraOrbitController* CameraOrbitController = Cast<ACPP_CameraOrbitController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+    CameraOrbitController->InputMode = EInputMode::GODMODEINPUT;
 
-    //ACPP_GameHUD* GameHUD = Cast<ACPP_GameHUD>(CameraOrbitController->GetHUD());
-    //GameHUD->GameWidget->SetVisibility(ESlateVisibility::Hidden);
-    //GameHUD->GameWidget->SetIsEnabled(false);
+    ACPP_GroundStationPawn* GroundStationPawn = Cast<ACPP_GroundStationPawn>(CameraOrbitController->GetPawn());
+    GroundStationPawn->GetComponentByClass(UCameraComponent::StaticClass())->Activate();
 
-    //TArray<AActor*> Satellites;
-    //UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACPP_Satellite::StaticClass(), Satellites);
-    //for (AActor* Satellite : Satellites)
-    //{
-    //    Satellite->SetActorHiddenInGame(false);
-    //}
+    ACPP_GameHUD* GameHUD = Cast<ACPP_GameHUD>(CameraOrbitController->GetHUD());
+    GameHUD->GameScreenWidget->SetVisibility(ESlateVisibility::Hidden);
+    GameHUD->GameScreenWidget->SetIsEnabled(false);
+
+    TArray<AActor*> Satellites;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACPP_Satellite::StaticClass(), Satellites);
+    for (AActor* Satellite : Satellites)
+    {
+        Satellite->SetActorHiddenInGame(false);
+    }
 }
 
 void UCPP_CheatManager::ClientView()
 {
-    //ACPP_CameraOrbitController* CameraOrbitController = Cast<ACPP_CameraOrbitController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
-    //CameraOrbitController->InputMode = EInputMode::GROUNDSTATIONINPUT;
+    ACPP_CameraOrbitController* CameraOrbitController = Cast<ACPP_CameraOrbitController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+    CameraOrbitController->InputMode = EInputMode::GROUNDSTATIONINPUT;
 
-    //ACPP_GameHUD* GameHUD = Cast<ACPP_GameHUD>(CameraOrbitController->GetHUD());
-    //GameHUD->GameWidget->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
-    //GameHUD->GameWidget->SetIsEnabled(true);
+    ACPP_GroundStationPawn* GroundStationPawn = Cast<ACPP_GroundStationPawn>(CameraOrbitController->GetPawn());
+    GroundStationPawn->GetComponentByClass(UCameraComponent::StaticClass())->Deactivate();
 
-    //TArray<AActor*> Satellites;
-    //UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACPP_Satellite::StaticClass(), Satellites);
-    //for (AActor* Satellite : Satellites)
-    //{
-    //    Satellite->SetActorHiddenInGame(true);
-    //}
+    ACPP_GameHUD* GameHUD = Cast<ACPP_GameHUD>(CameraOrbitController->GetHUD());
+    GameHUD->GameScreenWidget->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+    GameHUD->GameScreenWidget->SetIsEnabled(true);
+
+    TArray<AActor*> Satellites;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACPP_Satellite::StaticClass(), Satellites);
+    for (AActor* Satellite : Satellites)
+    {
+        Satellite->SetActorHiddenInGame(true);
+    }
 }
