c79180056aa5b926206ff512505af5977aecf10b
Daniel Francisco <deny.jo@hotmail.com>
2024-03-06 15:39:27 +0000

    Add new satellite model and replicate camera orbitable component

--- Source/SpaceWarfare/Public/CPP_CameraOrbitableComponent.h
+++ Source/SpaceWarfare/Public/CPP_CameraOrbitableComponent.h
@@ -13,26 +13,27 @@ class SPACEWARFARE_API UCPP_CameraOrbitableComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
     float StartOrbitDistance;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
     float MaxOrbitDistance;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
     float MinOrbitDistance;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
     float ZoomSpeed;
 
 	// Sets default values for this component's properties
 	UCPP_CameraOrbitableComponent();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
