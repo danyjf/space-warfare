b3933cf6ed4e272724e54c227ba20643002c532e
Daniel Francisco <deny.jo@hotmail.com>
2024-04-17 12:04:09 +0100

    Add spend and get fuel level functions

--- Source/SpaceWarfare/Private/CPP_FuelTank.cpp
+++ Source/SpaceWarfare/Private/CPP_FuelTank.cpp
@@ -8,6 +8,9 @@ UCPP_FuelTank::UCPP_FuelTank()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
+
+    MaxFuel = 100.0f;
+    FuelLevel = MaxFuel;
 }
 
 // Called when the game starts
@@ -20,4 +23,26 @@ void UCPP_FuelTank::BeginPlay()
 void UCPP_FuelTank::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+float UCPP_FuelTank::GetFuelLevel()
+{
+    return FuelLevel;
+}
+
+/**
+ * Spends fuel from the fuel tank.
+ * @param Value The amount of fuel to remove from the tank.
+ * @return  A boolean representing whether the fuel tank had enough fuel to spend.
+ *          If it didn't have enough, then the bool returns false and the fuel is not spent.
+*/
+bool UCPP_FuelTank::SpendFuel(float Value)
+{
+    if (FuelLevel - Value < 0.0f)
+    {
+        return false;
+    }
+
+    FuelLevel -= Value;
+    return true;
 }
--- Source/SpaceWarfare/Public/CPP_FuelTank.h
+++ Source/SpaceWarfare/Public/CPP_FuelTank.h
@@ -6,21 +6,31 @@
 #include "Components/ActorComponent.h"
 #include "CPP_FuelTank.generated.h"
 
-
 UCLASS( Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SPACEWARFARE_API UCPP_FuelTank : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxFuel;
+
+    UFUNCTION(BlueprintCallable)
+    bool SpendFuel(float Value);
+
+    UFUNCTION(BlueprintCallable)
+    float GetFuelLevel();
+
 	// Sets default values for this component's properties
 	UCPP_FuelTank();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+    float FuelLevel;
 };
